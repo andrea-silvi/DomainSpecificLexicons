3f00fa3942f9fe9d9ceac5c44df574683bd08490
andrea-silvi <andre_silvy@hotmail.it>
2021-12-21 17:37:47 +0100

    start of svm step

--- generateSeedData.py
+++ generateSeedData.py
@@ -8,13 +8,32 @@ from utils.utils import upload_args_from_json
 import numpy as np
 from AmazonDataset import parseDataset
 from sklearn.feature_extraction.text import CountVectorizer
+from liblinear.liblinearutil import train
 
 
 def generate_bow(data):
     vectorizer = CountVectorizer()
     X = vectorizer.fit_transform(data[:, 0])
     y = data[:, 1]
-    return X, y
+    return X, y, vectorizer.vocabulary_
+
+
+def train_linear_pred(X, y):
+    w_negative = len(y[y == +1])/len(y)
+    w_positive = 1-w_negative
+    m = train(y, X, f'-t 0 -w-1 {w_negative} -w+1 {w_positive}')
+    w = m.sv_coef
+    return w
+
+
+def assign_word_labels(X, w, vocabulary, f_min):
+    frequencies = X.sum(axis=0)
+    ind = np.where(frequencies<f_min)
+    filtered_vocabulary = {key:val for key, val in vocabulary.items() if val not in ind and not key.startswith('negatedw')}
+    for key, val in filtered_vocabulary.items():
+        #build dataset for part 2
+        continue
+
 
 
 if __name__ == '__main__':
