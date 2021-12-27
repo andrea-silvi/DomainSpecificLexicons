56a0daf5e9533e376c16fd59dcb30d6ab4f04197
Andrea Silvi <andrea.silvi@studenti.polito.it>
2021-12-27 19:25:46 +0100

    commented main

--- generateSeedData.py
+++ generateSeedData.py
@@ -19,30 +19,29 @@ def generate_bow(data):
 
 
 def train_linear_pred(X, y):
-    w_negative = len(y[y == +1])/len(y)
-    w_positive = 1-w_negative
+    w_negative = len(y[y == +1]) / len(y)
+    w_positive = 1 - w_negative
     m = train(y, X, f'-t 0 -w-1 {w_negative} -w+1 {w_positive}')
-    w = m.sv_coef
+    w = m.get_decfun_coef()
     return w
 
 
 def assign_word_labels(X, w, vocabulary, f_min):
     frequencies = X.sum(axis=0)
-    ind = np.where(frequencies<f_min)
-    filtered_vocabulary = {key:val for key, val in vocabulary.items() if val not in ind and not key.startswith('negatedw')}
+    ind = np.where(frequencies < f_min)
+    filtered_vocabulary = {key: val for key, val in vocabulary.items() if
+                           val not in ind and not key.startswith('negatedw')}
     for key, val in filtered_vocabulary.items():
-        #build dataset for part 2
+        # build dataset for part 2
         continue
 
 
-
 if __name__ == '__main__':
-    opt = upload_args_from_json(os.path.join("parameters", "generate_seed_data.json"))
+    '''opt = upload_args_from_json(os.path.join("parameters", "generate_seed_data.json"))
     if opt.isAmazon:
         data = parseDataset(opt)
     else:  # already a numpy array of shape (N, 2)
         pass
     processed_data = (data)
     X, y = generate_bow(processed_data)
-
-'this stuff is !notgood'
+    '''
