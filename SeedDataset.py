33a329644ecdba4a44146be4528211ac23664c29
Ulysse Marquis <ulysse.marquis@grenoble-inp.org>
2021-12-29 16:26:01 +0100

    building dataset loader

--- SeedDataset.py
+++ SeedDataset.py
@@ -2,19 +2,22 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 
+from utils.glove_loader import load_glove_model
+
 '''
-We need [word_vector, target] -> label propagation -> [word, target]'''
+We need [word_vector, target] -> label propagation -> [word, target]
+'''
 
 
 class SeedDataset(Dataset):
-    def __init__(self, data, targets, glove_embeddings):
-        self.data = data
-        self.targets = torch.Tensor(targets)
-        self.glove_embeddings = glove_embeddings
+    def __init__(self, data, embeddings_path):
+        self.words = list(data.keys())
+        self.scores = list(data.values())
+        self.embeddings = load_glove_model(embeddings_path, data)
 
     def __getitem__(self, index):
-        x = self.glove_embeddings[self.data[index]]
-        y = self.targets[index]
+        x = torch.Tensor(self.embeddings[self.data[index]])
+        y = torch.Tensor(self.scores[index])
 
         return x, y
 
