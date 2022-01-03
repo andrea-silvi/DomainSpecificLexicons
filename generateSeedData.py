1d1793c77a731d13d44dc832f87dceedc20f47c2
Andrea Silvi <andrea.silvi@studenti.polito.it>
2022-01-03 18:40:44 +0100

    fixed generateseeddata again

--- generateSeedData.py
+++ generateSeedData.py
@@ -36,7 +36,7 @@ def train_linear_pred(X, y, print_overfitting=False):
 
 def assign_word_labels(X, w, vocabulary, f_min):
     frequencies = X.sum(axis=0)
-    ind = np.where(frequencies < f_min)
+    ind = np.where(frequencies < f_min)[0]
     seed_data = {key: w[val] for key, val in vocabulary.items() if (val not in ind) and (not key.startswith('negatedw'))}
     non_seed_data = [key for key, val in vocabulary.items() if (val in ind) and (not key.startswith('negatedw'))]
     return SeedDataset(seed_data, EMBEDDINGS_PATH),  SeedDataset(non_seed_data, EMBEDDINGS_PATH, split='test')
