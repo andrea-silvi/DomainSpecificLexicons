check for words with wrong dimension (whitespaces)

utils/glove_loader.py
@@ -11,7 +11,8 @@ def load_glove_model(File, vocab=None):
                 word = split_line[0]
                 if vocab == None or word in vocab:
                     embedding = np.array(split_line[1:], dtype=np.float32)
-                    glove_model[word] = embedding
+                    if len(embedding) == 300:
+                        glove_model[word] = embedding
             except ValueError:
                 continue
 