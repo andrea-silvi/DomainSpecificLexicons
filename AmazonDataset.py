18ffcecf7c7024a9f4e9125a698783e2d2a35076
Andrea Silvi <andrea.silvi@studenti.polito.it>
2022-02-01 11:24:58 +0100

    stanfordparser negations to be tested

--- AmazonDataset.py
+++ AmazonDataset.py
@@ -1,8 +1,8 @@
 import gzip
 import json
 from nltk import RegexpTokenizer
-from utils.preprocessing import find_negations
-
+from utils.preprocessing import find_negations, find_complex_negations
+from nltk.parse.stanford import StanfordDependencyParser
 
 def parse(path):
     g = gzip.open(path, 'rb')
@@ -10,7 +10,7 @@ def parse(path):
         yield json.loads(l)
 
 
-def parse_dataset(dataset_name):
+def parse_dataset(dataset_name, complex_negations=False):
     """
     Generate a numpy array with (review, score) from a gzip file.
     We throw away reviews with scores = 3 and we consider all ones below 3 as negative, and all
@@ -18,10 +18,19 @@ def parse_dataset(dataset_name):
     """
     reviews, scores = [], []
     tokenizer = RegexpTokenizer(r'\w+')
+    if complex_negations:
+        jar_path = '/content/stanford-corenlp-4.2.2/stanford-corenlp-4.2.2.jar'
+        # Path to CoreNLP model jar
+        models_jar_path = '/content/stanford-corenlp-4.2.2-models-english.jar'
+        # Initialize StanfordDependency Parser from the path
+        parser = StanfordDependencyParser(path_to_jar=jar_path, path_to_models_jar=models_jar_path)
     for review in parse(dataset_name):
         try:
             if review["overall"] != 3.0:
-                rev = find_negations(review["reviewText"], tokenizer)
+                if not complex_negations:
+                    rev = find_negations(review["reviewText"], tokenizer)
+                else:
+                    rev = find_complex_negations(review["reviewText"], tokenizer, parser, negations_list=['not', 'nor', 'never'])
                 score = -1 if review["overall"] < 3.0 else +1
                 reviews.append(rev)
                 scores.append(score)
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -2,8 +2,29 @@ def find_negations(review, tokenizer):
     tokens = tokenizer.tokenize(review)
     clean_review = ''
     for i, t in enumerate(tokens):
-        if t == 'not' and i != len(tokens)-1:
+        if t == 'not' and i != len(tokens) - 1:
             tokens[i + 1] = 'NEGATEDW' + tokens[i + 1]
         else:
             clean_review = clean_review + ' ' + t.lower()
+    return clean_review.strip()
+
+
+def find_complex_negations(review, tokenizer, parser, negations_list):
+    tokens = tokenizer.tokenize(review)
+    if 'not' in tokens:
+        result = parser.raw_parse(review)
+        dependency = result.__next__()
+        for dep in list(dependency.triples()):
+            if str(dep[2][0]) in negations_list:
+                for i, token in enumerate(tokens):
+                    if token == str(dep[0][0]):
+                        tokens[i] = 'NEGATEDW' + tokens[i]
+                        break
+                    elif token == 'NEGATEDW' + str(dep[0][0]):
+                        tokens[i] = str(dep[0][0])
+                        break
+        tokens = list(filter(lambda w: w not in negations_list, tokens))
+    clean_review = ''
+    for t in tokens:
+        clean_review = clean_review + ' ' + t.lower()
     return clean_review.strip()
