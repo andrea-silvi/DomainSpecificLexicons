dcbe022a2469bddeaa0cb9e0a02b09dc9f2fd2e7
ulysse <ulysse.marquis@grenoble-inp.org>
2022-02-03 01:30:22 +0100

    AmazonDataset - add whole sentence negation handling

--- AmazonDataset.py
+++ AmazonDataset.py
@@ -10,25 +10,34 @@ def parse(path):
         yield json.loads(l)
 
 
-def parse_dataset(dataset_name, complex_negations=False):
+# TODO : change negation type handling (not using booleans, rather enum type or something similar)
+def parse_dataset(dataset_name, complex_negations=False, whole_sentence_negation=False):
     """
     Generate a numpy array with (review, score) from a gzip file.
     We throw away reviews with scores = 3 and we consider all ones below 3 as negative, and all
     ones above 3 as positive.
+    @ params : complex_negations and whole_sentence_negation cannot both be True
     """
+    assert not (complex_negations and whole_sentence_negation), "complex_negations and whole_sentence_negation cannot both be True"
     reviews, scores = [], []
     tokenizer = RegexpTokenizer(r'\w+')
+
     if complex_negations:
         jar_path = '/content/stanford-corenlp-4.2.2/stanford-corenlp-4.2.2.jar'
         # Path to CoreNLP model jar
         models_jar_path = '/content/stanford-corenlp-4.2.2-models-english.jar'
         # Initialize StanfordDependency Parser from the path
         parser = StanfordDependencyParser(path_to_jar=jar_path, path_to_models_jar=models_jar_path)
+
+    
     for review in parse(dataset_name):
         try:
             if review["overall"] != 3.0:
                 if not complex_negations:
-                    rev = find_negations(review["reviewText"], tokenizer)
+                    if whole_sentence_negation:
+                        rev = whole_sentence_negation(review["reviewText"], tokenizer)
+                    else:
+                        rev = find_negations(review["reviewText"], tokenizer)
                 else:
                     rev = find_complex_negations(review["reviewText"], tokenizer, parser, negations_list=['not', 'nor', 'never'])
                 score = -1 if review["overall"] < 3.0 else +1
