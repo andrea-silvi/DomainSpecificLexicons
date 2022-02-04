479c518d63a2e1c1aa94e9da0375626e0371a1e7
Andrea Silvi <andrea.silvi@studenti.polito.it>
2022-02-04 10:36:57 +0100

    trying to download and instanciate parser model

--- AmazonDataset.py
+++ AmazonDataset.py
@@ -3,12 +3,27 @@ import json
 from nltk import RegexpTokenizer
 from utils.preprocessing import find_negations, find_complex_negations
 from nltk.parse.stanford import StanfordDependencyParser
+import wget
+import zipfile
 
 def parse(path):
     g = gzip.open(path, 'rb')
     for l in g:
         yield json.loads(l)
 
+
+def instanciate_parser():
+    wget.download('https://nlp.stanford.edu/software/stanford-corenlp-4.2.2.zip')
+    wget.download('https://nlp.stanford.edu/software/stanford-corenlp-4.2.2-models-english.jar')
+
+    with zipfile.ZipFile('/content/stanford-corenlp-4.2.2.zip', 'r') as zip_ref:
+        zip_ref.extractall('/content/stanford-corenlp-4.2.2')
+    jar_path = '/content/stanford-corenlp-4.2.2/stanford-corenlp-4.2.2.jar'
+    # Path to CoreNLP model jar
+    models_jar_path = '/content/stanford-corenlp-4.2.2-models-english.jar'
+    # Initialize StanfordDependency Parser from the path
+    parser = StanfordDependencyParser(path_to_jar=jar_path, path_to_models_jar=models_jar_path)
+    return parser
 
 def parse_dataset(dataset_name, complex_negations=False):
     """
@@ -19,11 +34,8 @@ def parse_dataset(dataset_name, complex_negations=False):
     reviews, scores = [], []
     tokenizer = RegexpTokenizer(r'\w+')
     if complex_negations:
-        jar_path = '/content/stanford-corenlp-4.2.2/stanford-corenlp-4.2.2.jar'
-        # Path to CoreNLP model jar
-        models_jar_path = '/content/stanford-corenlp-4.2.2-models-english.jar'
-        # Initialize StanfordDependency Parser from the path
-        parser = StanfordDependencyParser(path_to_jar=jar_path, path_to_models_jar=models_jar_path)
+        parser = instanciate_parser()
+
     for review in parse(dataset_name):
         try:
             if review["overall"] != 3.0:
