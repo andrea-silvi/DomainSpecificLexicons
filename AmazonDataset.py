ce91097d5373eb313455ff057a35d1e2e69561b1
Andrea Silvi <andrea.silvi@studenti.polito.it>
2022-02-05 11:16:19 +0100

    print distribution of reviews over years

--- AmazonDataset.py
+++ AmazonDataset.py
@@ -1,8 +1,9 @@
+import argparse
 import gzip
 import json
 from nltk import RegexpTokenizer
 from utils.preprocessing import find_negations, whole_sentence_negation
-from nltk.parse.stanford import StanfordDependencyParser
+import seaborn as sns
 
 def parse(path):
     g = gzip.open(path, 'rb')
@@ -67,3 +68,28 @@ def parse_dataset_by_year(dataset_name, cluster, negation='normal'):
             continue
 
     return reviews, scores
+
+def distributionOverYears(dataset_name):
+    """
+    Get the dataset distribution of reviews over the years.
+    """
+    reviewsPerYear = {}
+    for review in parse(dataset_name):
+        try:
+            year_review = int(review["reviewTime"].split(",")[1][1:])
+            if year_review in reviewsPerYear:
+                reviewsPerYear[year_review] += 1
+            else:
+                reviewsPerYear[year_review] = 1
+        except KeyError:
+            continue
+    for k, v in reviewsPerYear.items():
+        print(f'N. of reviews for year {k}: v.')
+        plt = sns.histplot(data=reviewsPerYear)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_name', type=str, required=True, help='Path of the dataset.')
+    args = parser.parse_args()
+    distributionOverYears(args.dataset_name)
