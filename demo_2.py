24c5e60d40849a0e6395f86b31bfb6d01c63318f
Andrea Silvi <andrea.silvi@studenti.polito.it>
2022-02-05 10:55:40 +0100

    demo 2 should work

---
+++ demo_2.py
@@ -0,0 +1,141 @@
+import argparse
+from AmazonDataset import parse_dataset, parse_dataset_by_year
+from SeedDataset import SeedDataset
+from generateSeedData import generate_bow, get_frequencies, train_linear_pred, assign_word_labels
+from train import train, predict
+import numpy as np
+import neptune.new as neptune
+import json
+import time
+from utils.glove_loader import load_glove_words
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
+
+def perform(texts, scores, args, cluster = None):
+
+    start = time.time()
+    y = np.array(scores)
+    X, vocabulary = generate_bow(texts)
+    frequencies = get_frequencies(X)
+    print(f'review-word bow matrix generated in {int(time.time() - start)} seconds.')
+    start = time.time()
+    W = train_linear_pred(X, y)
+    print(f'found linear coefficients in {int(time.time() - start)} seconds.')
+
+    glove_words = load_glove_words(EMBEDDINGS_PATH)
+    seed_dataset = assign_word_labels(frequencies, W, vocabulary, f_min=args.f_min,
+                                      EMBEDDINGS_PATH=EMBEDDINGS_PATH, glove_words=glove_words)
+    print('start of training...')
+    start = time.time()
+
+
+    model = train(seed_dataset, run)
+    print(f'time of training: {int(time.time() - start)} seconds')
+    complete_results = seed_dataset.get_dictionary()
+
+    non_seed_data = {w: 0 for w in glove_words if w not in complete_results}
+    non_seed_dataset = SeedDataset(non_seed_data, EMBEDDINGS_PATH, split='test')
+    start = time.time()
+    results = predict(model, non_seed_dataset)
+    print(f'time of predictions: {int(time.time() - start)} seconds')
+    complete_results.update(results)
+    # close the run on neptune
+
+    complete_results_to_scaled = list(complete_results.values())
+    scaled = np.interp(np.array(complete_results_to_scaled),
+                       (np.min(complete_results_to_scaled), np.max(complete_results_to_scaled)), (-1, +1)).astype(
+        "float32")
+    """
+    for i, (k, v) in enumerate(complete_results.items()):
+        if i > 20:
+            break
+        print(k, scaled[i])
+    """
+    max_n = min(len(scaled), 10)
+
+    indices_high = (-scaled).argsort()[:max_n]
+    indices_low = (scaled).argsort()[:max_n]
+    words = list(complete_results.keys())
+
+    #save the top and bottom words score pairs
+    top = []
+    bottom = []
+    print(f"THE 15 MOST HIGH")
+    for i in range(len(indices_high)):
+        #if i == 0:
+            #run["sys/tags"].add([f"max: {words[indices_high[i]]} : {scaled[indices_high[i]]} "])
+        print(f"\n{i} {words[indices_high[i]]} : {scaled[indices_high[i]]}")
+        top.append([words[indices_high[i]],scaled[indices_high[i]] ])
+
+    print(f"THE 15 MOST LOW")
+    for i in range(len(indices_low)):
+        #if i == 0:
+            #run["sys/tags"].add([f"min: {words[indices_low[i]]} : {scaled[indices_low[i]]} "])
+        print(f"\n{i} {words[indices_low[i]]} : {scaled[indices_low[i]]}")
+        bottom.append([words[indices_low[i]],scaled[indices_low[i]] ])
+
+    mean_value = np.mean(scaled)
+    print(f"Mean of the lexicon {mean_value} ")
+
+    #we filter out the values [-0.2, 0.2]
+    distribution_filtered = list(filter(lambda x: (x > mean_value+0.2) and (x < mean_value - 0.2), scaled))
+    plt = sns.displot(distribution_filtered, kind="kde")
+    if cluster == None:
+        plt.savefig("Distribution_words_for_score.png")
+    else:
+        plt.savefig(f"Distribution_words_for_score_{cluster}.png")
+    return cluster, top, bottom
+
+
+EMBEDDINGS_PATH = '/content/drive/MyDrive/glove.840B.300d.txt'
+if __name__ == '__main__':
+    start = time.time()
+    file_parameters = open("neptune.json")
+    parameters = json.load(file_parameters)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_name', type=str, required=True, help='Path of the dataset.')
+    parser.add_argument('--f_min', type=int, required=True, help='frequency threshold in seed data generation.')
+    parser.add_argument('--user', type=str, required=True, help='user to log stuff into his neptune.')
+    parser.add_argument('--neg', type=str, required=True, help='more costly method to find better negations.')
+    parser.add_argument("--second_extension", type = bool, required=False, help ="use the ablation study version", default=False)
+    args = parser.parse_args()
+
+    #start the neptune monitoring
+    neptune_parameters = parameters[args.user]
+
+    run = neptune.init(api_token=neptune_parameters["neptune_token"],
+                       project=neptune_parameters["neptune_project"])  # pass your credentials
+
+    #in this part we check if we want to perform the second task sa
+    run["sys/tags"].add([f"f-min: {args.f_min}"])
+
+    if bool(args.second_extension):
+        run["sys/tags"].add([f"ablation"])
+        years = list(range(1995, 2015))
+        clustered_years = list(split(years, 4))
+
+        #for each cluster of years we perform the process
+        for cluster in clustered_years:
+            texts, scores = parse_dataset_by_year(args.dataset_name, cluster)
+            if len(texts) != 0:
+                #TODO manage short dataset
+                print(f"CLUSTER {cluster} with length {len(texts)}")
+                cluster, top, bottom = perform(texts, scores, args, cluster )
+            else:
+                print(f"CLUSTER  {cluster} IS EMPTY")
+
+
+    else:
+
+        print('the arguments are ', args)
+        texts, scores = parse_dataset(args.dataset_name, True if args.neg == 'complex' else False,
+                                      args.second_extension)
+        print(f'dataset has been read in {int(time.time() - start)} seconds.')
+
+        perform(texts, scores, args)
+
+    run.stop()
