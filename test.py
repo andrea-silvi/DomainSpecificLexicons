f7f6bf8aed99d843bfb18636adaea1088eb3c6bc
Andrea Silvi <andrea.silvi@studenti.polito.it>
2022-02-05 17:22:53 +0100

    changed threshold to frac_negatives*100 th percentile

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from nltk import RegexpTokenizer
 from sklearn.metrics import precision_recall_fscore_support
 from testDatasets import parseIMDBDataset, parseGameStopDataset
-
+import numpy as np
 
 def calculate_sentiment(tokens, lexicon):
     pred = 0
@@ -13,7 +13,8 @@ def calculate_sentiment(tokens, lexicon):
 def calculate_statistics(df, tokenizer, lexicon):
     df['review'] = df['review'].apply(lambda x: tokenizer.tokenize(x.lower()))
     df['prediction'] = df['review'].apply(lambda x: calculate_sentiment(x, lexicon))
-    threshold = (df['prediction'].sum())*((df["sentiment"] == -1).sum()/len(df["sentiment"]))
+    fraction_negatives = (df["sentiment"] == -1).sum()/len(df["sentiment"])
+    threshold = np.percentile(df['prediction'], 100*fraction_negatives)
     df['prediction'] = df['prediction'].apply(lambda x: -1 if x < threshold else 1)
     print(f'Accuracy: {(df["prediction"] == df["sentiment"]).sum() / len(df["prediction"])}')
     scores = precision_recall_fscore_support(df["sentiment"], df["prediction"])
