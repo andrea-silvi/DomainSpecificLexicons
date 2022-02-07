c9e31170b8db2e6259ec860ba1fa4d9d55d8a2c6
Andrea Silvi <andrea.silvi@studenti.polito.it>
2022-02-07 05:59:23 +0100

    fixed gamestop balanced dataset reading

--- testDatasets.py
+++ testDatasets.py
@@ -3,10 +3,6 @@ import pandas as pd
 
 def parseGameStopDataset(dataset_path='/content/drive/MyDrive/gamestop_balanced.csv'):
     df = pd.read_csv(dataset_path)
-    df = df.loc[:, ['review_description', 'rating']]
-    df = df[(df['rating'] != 3)]
-    df['rating'] = df['rating'].apply(lambda x: -1 if x < 3 else 1)
-    df.rename(columns={'review_description': 'review', 'rating': 'sentiment'}, inplace=True)
     return df
 
 
