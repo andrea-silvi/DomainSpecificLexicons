48d4ab2401a2216dcf465922fcf86b7fb2484cf8
Andrea Silvi <andrea.silvi@studenti.polito.it>
2022-02-08 22:49:17 +0100

    fixed expereiment 3

--- trainSubreddits.py
+++ trainSubreddits.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         print("ATTENTION! List of subreddits to build lexicons of needed. Restart.")
     # for each subreddit we create the lexicon
     for i, subreddit in enumerate(subreddits):
-        lexicon = createLexicon(arguments, subreddit)
+        lexicon = createLexicon(arguments, subreddit=subreddit)
         if i == 0:
             final_dataframe = pd.DataFrame.from_dict(lexicon, orient='index', columns=[subreddit])
         else:
