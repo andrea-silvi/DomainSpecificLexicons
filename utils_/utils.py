934dc491a6f141e619376b0a802543068101d033
Andrea Silvi <andrea.silvi@studenti.polito.it>
2022-02-13 23:50:33 +0100

    moved out experiments

--- utils_/utils.py
+++ utils_/utils.py
@@ -31,7 +31,7 @@ def arguments_parsing():
                         help='either not use or use scores of negated word in seed data induction step.',
                         default='normal', choices=['normal', "whole"])
     parser.add_argument('--exp', type=str, required=True, help='Type of experiment.',
-                        choices=['exp1', 'exp2', 'exp3'])
+                        choices=['exp1', 'exp2', 'exp3', 'no_experiment'])
     parser.add_argument('--embeddings', type=str, required=True, help='file path of word vector embeddings.')
     parser.add_argument('--IMDB', type=str, required=False, help='file path of IMDB dataset for experiment 1.')
     parser.add_argument('--GameStop', type=str, required=False, help='file path of GameStop dataset for experiment 1.')
